4d3f61da865245212dc67f448ee557d5a85f8844
= <=>
2017-10-11 17:22:05 +0300

    add new classes(waiter, orderplace) in models

--- api/admin.py
+++ api/admin.py
@@ -2,20 +2,28 @@ from django.contrib import admin
 from api.models import *
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price') # какие свойства Item должны показываться в админке
+    list_display = ('id', 'item_name', 'price') # какие свойства Item должны показываться в админке
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'category_name')
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'date')
+    list_display = ('id', 'order_place', 'waiter', 'date')
 
 class OrderLineAdmin(admin.ModelAdmin):
     list_display = ('id', 'item', 'order', 'count')
+
+class WaitersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'waiter_name', 'work_place')
+
+class OrderPlaceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'place_name')
 
 
 admin.site.register(Item, ItemAdmin) # чтобы модель Item стала доступна на стр админки
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderLine, OrderLineAdmin)
+admin.site.register(Waiters, WaitersAdmin)
+admin.site.register(OrderPlace, OrderPlaceAdmin)
 
--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from datetime import *
 from django.utils import timezone
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Название категории')
+    category_name = models.CharField(max_length=255, verbose_name='Название категории')
     parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
     alias = models.SlugField(verbose_name='Alias категории')
 
@@ -12,11 +12,11 @@ class Category(models.Model):
         verbose_name_plural = 'Категории'
 
     def __str__(self):
-        return 'Категория %s' % self.name
+        return 'Категория %s' % self.category_name
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=255,
+    item_name = models.CharField(max_length=255,
                             verbose_name='Название товара')  # verbose_name как товар будет отображаться в админке
     price = models.IntegerField(default=0, verbose_name='Цена')
     alias = models.SlugField(verbose_name='Alias товара')
@@ -25,15 +25,35 @@ class Item(models.Model):
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
-    #
 
     def __str__(self):
-        return 'Товар %s' % self.name
+        return 'Товар %s' % self.item_name
+
+class OrderPlace(models.Model):
+    place_name = models.CharField(max_length=255, null=True, verbose_name="Название ресторана")
+
+    class Meta:
+        verbose_name = 'Название ресторана'
+        verbose_name_plural = 'Названия ресторанов'
+
+    def __str__(self):
+        return 'Ресторан %s' % self.place_name
+
+
+class Waiters(models.Model):
+    waiter_name = models.CharField(max_length=255, null=True, verbose_name="Имя официанта")
+    work_place = models.ForeignKey(OrderPlace, null=True, verbose_name="Место работы")
+    class Meta:
+        verbose_name = 'Имя официанта'
+        verbose_name_plural = 'Имена официантов'
+
+    def __str__(self):
+        return 'Официанта %s' % self.waiter_name
 
 
 class Order(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Имя официанта')
-    place = models.CharField(max_length=255, verbose_name='Место', null=True)
+    waiter = models.ForeignKey(Waiters, null=True)
+    order_place = models.ForeignKey(OrderPlace, null=True)
     date = models.DateTimeField(default=timezone.now, verbose_name='Дата заказа')
     items = models.ManyToManyField(Item, through='OrderLine')
 
